6c01bd9561984f1c9c207eb4fb0d474c463c777a
Lucas Feldmann <lucfeldmann23@googlemail.com>
2020-08-06 10:37:04 +0200

    fixed izhikevich model

--- src/models/neurons/izhikevich.py
+++ src/models/neurons/izhikevich.py
@@ -1,17 +1,14 @@
-from brian2 import mV, ms, volt, second
 from brian2 import NeuronGroup
 
-def create_izhikevich_neuron(a,b,c,d,Vmax):
-    """Creates a brian2 NeuronGroup that contains a single izhikevich neuron with the given parameters"""
+def create_izhikevich_neuron(Vmax = 35):
+    """Creates a brian2 NeuronGroup that contains a single izhikevich neuron"""
     # Define differential equation for izhikevich neuron
-    eqs = '''dvm/dt = (0.04/ms/mV)*vm**2+(5/ms)*vm+140*mV/ms-w + I : volt
-            dw/dt = a*(b*vm-w) : volt/second
-            I : volt/second'''
+    eqs = '''   dvm/dt = (0.04/ms/mV)*vm**2+(5/ms)*vm+140*mV/ms-w + I : volt
+                dw/dt = a*(b*vm-w) : volt/second
+                I : volt/second '''
     # Define reset function
-    reset = '''vm = c
-                w = w + d'''
-    # Set parameters
-    a = a/ms; b = b/ms; c = c * mV; d = d * volt/second
+    reset = ''' vm = c
+                w = w + d '''    
     # Define threshold
     threshold = 'vm>{}*mV'.format(Vmax)
     # Return NeuronGroup object
