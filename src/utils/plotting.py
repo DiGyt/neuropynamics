6216fa306196df174e17ffb6774c5a057bc97141
Lucas Feldmann <lucfeldmann23@googlemail.com>
2020-08-10 12:22:08 +0200

    updated plotting functions

--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -3,14 +3,45 @@ import seaborn as sns
 
 # Apply default seaborn settings
 sns.set()
+sns.set_style({'axes.grid' : False})
 
-def create_default_plot(x, y, title = '', x_label = '', y_label = ''):
-    # Define plotting area and set size
-    plt.figure(figsize=(14,8))
-    # Create lineplot of the data
-    sns.lineplot(x, y)
-    # Set labels
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
+# Plotting function
+def create_default_plot(x, neuron_data, neuron_labels, neuron_colors, spikes = None, spike_color = 'steelblue', input_current = None, input_label = 'Input Current', input_color = 'gold', y_range = None, title = '', x_axis_label = '', y_axis_label = '', input_axis_label = 'Input Current (A)', zeroline = True):
+    
+    # Create first y axis and set size of figure
+    fig, ax1 = plt.subplots(figsize=(14,6))
+
+    # Set axis labels
+    ax1.set_xlabel(x_axis_label)
+    ax1.set_ylabel(y_axis_label)
+
+    # Set y range if given
+    if y_range is not None:
+        ax1.set_ylim(y_range)
+
+    # Add horizontal 0 line
+    if zeroline:
+        ax1.axhline(y = 0, linestyle = '--', linewidth = 1, color = 'gray')
+
+    # Plot a line for each neuron datapoint
+    for idx, y in enumerate(neuron_data):
+        ax1.plot(x, y, color = neuron_colors[idx], label = neuron_labels[idx])
+
+    # Plot spikes if given
+    if spikes is not None:
+        ax1.scatter(spikes[0], spikes[1], color = spike_color, label = 'Spikes')
+
+    # Show input current if given
+    if input_current is not None:
+        # Create 2nd y-axis
+        ax2 = ax1.twinx()
+        # Set axis label
+        ax2.set_ylabel(input_axis_label)
+        # Draw input line
+        ax2.plot(x, input_current, color = input_color, label = input_label)   
+    
+    # Add legend
+    fig.legend(loc="upper right", bbox_to_anchor=(0.825, 0.8))
+
     # Set title
     plt.title(title)
