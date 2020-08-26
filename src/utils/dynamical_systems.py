5f42d89e8bf62516f8ce6fcb1846e104dbf6559c
Lucas Feldmann <lucfeldmann23@googlemail.com>
2020-08-26 17:53:09 +0200

    Revert "fixed wrong function names"
    This reverts commit 51c8f111f97f0b03328bbfd35a2cdd1dcb5a9d79.

--- src/utils/dynamical_systems.py
+++ src/utils/dynamical_systems.py
@@ -17,7 +17,7 @@ def rk4( f, x0, y0, h, xn ):
         
     return y, x
 
-def plot_equation(eq, solvar, plotvar, inputrange):
+def plotEquation(eq, solvar, plotvar, inputrange):
     '''
         Given a sympy equation object `eq`, this function return a series of values,
         in the range of `inputrange` such that the equation is solved for
@@ -32,7 +32,7 @@ def plot_equation(eq, solvar, plotvar, inputrange):
 
     return vals
 
-def find_fixed_points(f, g, xrange, yrange):
+def findFixedPoints(f, g, xrange, yrange):
     return [(x, y) for x in xrange for y in yrange if f(x, y) == 0 and g(x, y) == 0]
 
 
